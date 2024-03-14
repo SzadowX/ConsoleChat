be10531378c4b95d759587a186796310c2258f8b
ShadowX <56553389+SzadowX@users.noreply.github.com>
2024-03-14 19:51:53 +0700

    Add files via upload

--- Windows/User.h
+++ Windows/User.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 #include <string>
 #include <algorithm> // для std::copy_n
--- Windows/Userlist.cpp
+++ Windows/Userlist.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <algorithm> // для std::copy_n
 #include "Userlist.h"
