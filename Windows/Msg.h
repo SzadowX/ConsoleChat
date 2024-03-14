849bb0ec4cc15599c6c972351a760cb6a9804614
ShadowX <56553389+SzadowX@users.noreply.github.com>
2024-03-14 19:28:34 +0700

    Update Msg.h

--- Windows/Msg.h
+++ Windows/Msg.h
@@ -20,38 +20,3 @@ public:
 	void getPrivate(std::string l, std::string r); // вывод личных сообщений
 	void getGroup(); // вывод групповых сообщений
 };
-
-/*
-class Dialogs{
-private:
-	int _msgCount{};
-	Msg *_msgListp{};
-public:
-	Dialogs(){};
-	Dialogs(int msgCount) : _msgCount(msgCount){
-		_msgListp = new Msg[_msgCount]{};
-		_msgListp[0] = Msg(std::string("user1"), std::string("Добро пожаловать!"));	
-	}
-	
-	~Dialogs(){
-		delete[] _msgListp;
-	}
-
-	void ShowM(){
-		for(int i = 0; i < _msgCount; i++){
-			_msgListp[i].showMsg();
-		}
-	}
-};
-*/
-
-//	void showMsgs(){ // вывод истории сообщений
-//		for(int i = 0; i < _msgc; i++){
-//			_msglist[i].getMsg();
-//		}
-//	}
-//	void addMsg(std::string u, std::string m){
-//		_msglist[_msgc - 1] = Msg(u, m);
-//		
-//	}
-//};
