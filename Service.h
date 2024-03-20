80db9cd39bcf914b5be2344a1342bfba46646ca5
George <161114343+G1VC@users.noreply.github.com>
2024-03-20 17:56:09 +0500

    Add files via upload

---
+++ Service.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <class T>
+class Info
+{
+private:
+   // T m_Obj;
+public:
+   char Inf_O();
+   void _Info_messages();
+   void _Info_m_1();
+   string _Info_m_2(string un);
+};
+
+template <class T>
+class TestConditins
+{
+private:
+	// T m_Obj;
+public:
+	char Inf_O();
+	void _Info_messages();
+	void _Info_m_1();
+	string _Info_m_2(string un);
+};
